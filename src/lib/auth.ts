0994c36ebe5cf452c127250c3e65314634a3ab9d
Marlus Marcos <marlus.marcos.018@ufrn.edu.br>
2024-11-04 16:29:50 -0300

    add gitignore pk

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@
 import axios from "axios";
 import jwt from "jsonwebtoken";
 import { JwtPayload } from "./types";
+import {pk} from "./publickey"
 
 export async function getTokenFromCode(code: string): Promise<{ access_token: string; refresh_token: string } | null> {
   try {
@@ -51,15 +52,7 @@ export function decodeToken(token: string | null): JwtPayload | null {
 }
 
 export async function verifyTokenSignature(token: string): Promise<boolean> {
-  const publicKey = `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnKx4rXjnjXyIkh8WOWDZ
-zLgwqfONM2FThpsSkbV0qUOU5Y4rZRHIhCgNg5alYPrI7qNayVTkB4CWL4jpHw9y
-Z9CFDRhty/eOrd4fNRnQ/lWQ9T8B6ghES9hSbWIurX5z6k+/fmQ+BaVhbu7CEbqB
-eQiLuFdsWH/nAcb6M+BOt/6HGJYj9oymp0sLA9Y2KQcR3WyyCY6IQnqW2CRQpAmY
-YC9WPQb+ZcKoYBygW/fja6irkmBMngVi+LBEbtPor5M9AhD5fVicAU7ZE4bKR2bp
-NocshcSLy+0ISThWY3iG2nnlSwqwYDF7YPE8RBTXN+JZNg8vzJGokg8lq+nGzIKN
-SQIDAQAB
------END PUBLIC KEY-----`
+  const publicKey = pk
   return new Promise((resolve) => {
     jwt.verify(token, publicKey, (err) => {
       if (err) {
